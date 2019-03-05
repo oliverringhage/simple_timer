c0b185af28465a076e5fc6e2d73e415137ab98df
Oliver <oliverringhage@gmail.com>
2019-03-05 18:17:07 +0100

    Changed the regex a bit

--- timer.py
+++ timer.py
@@ -3,12 +3,9 @@ import re
 import sys
 
 total_time = str(sys.argv[1])
-if(re.search("^[\d]{1,3}m[\d]{1,2}s$", total_time)):
+if(re.search("^[\d]{1,5}m[0-5]?[0-9]s$", total_time)):
     mins = int(total_time[:total_time.find('m')])
     secs = int(total_time[total_time.find('m') + 1 :total_time.find('s')])
-    if(secs > 59):
-        print("There cannot be more than 59 seconds")
-        sys.exit(0)
     while(mins > 0 or secs > 0):
         print("%2d minutes and %2d seconds" % (mins, secs))
         time.sleep(1)
@@ -17,4 +14,4 @@ if(re.search("^[\d]{1,3}m[\d]{1,2}s$", total_time)):
             mins = mins - 1
     print("\n\n\n\n\nTime is up")
 else:
-    print("Input in the wrong form, should be: xxmxxs")
+    print("Input in the wrong form, should be: xxmxxs, or minutes > 59")
